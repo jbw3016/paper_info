f8c512dc45fb0dc3321628b6b84f93ce0b87e097
Jongbok Won <wvnvwn@gmail.com>
2024-05-09 13:01:07 +0900

    test_01.py

---
+++ ACL/2023/test_01.py
@@ -0,0 +1,15 @@
+import xml.etree.ElementTree as ET
+import os
+import pandas as pd
+from function_module import load_paper_info
+
+data_dir = r'C:\Users\원종복\Desktop\paper_list\acl\acl-anthology\data\xml'
+year = 2023
+conference_type = 'acl'
+target_volume_id = 'long'
+
+paper_info = load_paper_info(data_dir=data_dir, year=year,
+                             conference_type=conference_type,
+                             target_volume_id=target_volume_id)
+
+print(paper_info.head())
