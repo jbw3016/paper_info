dac1c5c609cf4a855b07f4e61b099be2b12475a0
Jongbok Won <wvnvwn@gmail.com>
2024-05-10 09:13:29 +0900

    additional change in progress

--- ACL/2023/test_01.py
+++ ACL/2023/test_01.py
@@ -12,4 +12,8 @@ paper_info = load_paper_info(data_dir=data_dir, year=year,
                              conference_type=conference_type,
                              target_volume_id=target_volume_id)
 
-print(paper_info.head())
+save_path = r'C:\Users\원종복\Desktop\paper_info\ACL\2023'
+
+paper_info.to_csv(save_path, index=False)
+
+# print(paper_info.head())
